dae0fb7003ee56855ac98c035686cc8861537d09
Gunnar R <gunnarrosenberg@gmail.com>
2021-08-05 17:44:58 -0700

    first commit

---
+++ lib/csv_filter.ex
@@ -0,0 +1,18 @@
+defmodule CsvFilter do
+  @moduledoc """
+  Documentation for `CsvFilter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsvFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csv_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule CsvFilterTest do
+  use ExUnit.Case
+  doctest CsvFilter
+
+  test "greets the world" do
+    assert CsvFilter.hello() == :world
+  end
+end
