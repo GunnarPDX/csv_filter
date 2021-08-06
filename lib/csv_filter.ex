9bbb19cc7217a183c4550b162e722090706cf2be
Gunnar R <gunnarrosenberg@gmail.com>
2021-08-05 17:55:11 -0700

    read file stream

--- lib/csv_filter.ex
+++ lib/csv_filter.ex
@@ -4,15 +4,32 @@ defmodule CsvFilter do
   """
 
   @doc """
-  Hello world.
+  Reduce CSV
 
   ## Examples
-
-      iex> CsvFilter.hello()
-      :world
-
+      iex> reduce_csv(file_path, output_file_path, unique: ["Email", "Phone"])
+      :ok
   """
-  def hello do
-    :world
+  # CsvFilter.reduce_csv("./test/data/users_sample.csv", "./test/data/output_sample.csv", unique: ["Email"])
+
+  # %{
+  #  "" => ["", "", "", ""],
+  #  "Email" => "145@email.com",
+  #  "Name" => "John Doe ",
+  #  "Phone" => "5555555555"
+  # }
+
+  def reduce_csv(file_path, output_file_path, opts) when is_list(opts) do
+    opts = Enum.into(opts, %{})
+
+    output_file = File.open!(output_file_path, [:write, :utf8])
+
+    updated_file =
+      file_path
+      |> File.stream!()
+      |> CSV.decode(headers: true)
+      |> Enum.each(fn x -> IO.inspect(x) end)
+
+    :ok
   end
 end
